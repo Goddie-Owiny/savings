dfd2598b09cabd227479e7d1d15db5adf9502a74
Goddie-Owiny <goddiegodfreyz07gmail.com>
2024-08-03 01:06:36 +0300

    add pagination and some styles to the dashboard

--- savingsApp/views.py
+++ savingsApp/views.py
@@ -4,19 +4,41 @@ from django.http import HttpResponseBadRequest
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.contrib.auth import logout
+from django.db.models import Q
 from . models import *
 from . forms import *
 
 # Create your views here.
 @login_required
 def dashboard(request):
-    member_list = Member.objects.all().order_by('id')
+    total_members = Member.objects.count()
+    total_savings = Save.objects.aggregate(total_savings=models.Sum('amount'))['total_savings'] or 0
+    average_savings = Save.objects.aggregate(average_savings=models.Avg('amount'))['average_savings'] or 0
+
+    search_query = request.GET.get('search', '')
+    if search_query:
+        member_list = Member.objects.filter(
+            Q(name__icontains=search_query) |
+            Q(contact__icontains=search_query)
+        ).order_by('id')
+    else:
+        member_list = Member.objects.all().order_by('id')
+    
     paginator = Paginator(member_list, 10)  # Show 10 members per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'savingsApp/dashboard.html', {'page_obj': page_obj} )
 
-@login_required
+    context = {
+        'total_members': total_members,
+        'total_savings': total_savings,
+        'average_savings': average_savings,
+        'page_obj': page_obj,
+        # other context variables...
+    }
+    
+    return render(request, 'savingsApp/dashboard.html', context)
+
+
 @login_required
 def mem_reg(request):
     form = MemberForm()
